c805e9cf8d4858e7ae897fab5af9e9bc8b90e2ab
Michael Williams <mwilliams2964@gmail.com>
2020-01-21 13:14:03 -0700

    Updates

---
+++ precache-manifest.9aecd13ca228fa2ee060c92ff5f20450.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07f9335e0e0fe24c48e27dd6ad233bcf",
+    "url": "/google-books-search/index.html"
+  },
+  {
+    "revision": "b678142f649803aed036",
+    "url": "/google-books-search/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "91c4a5faee453b1d1758",
+    "url": "/google-books-search/static/js/2.42b16017.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/google-books-search/static/js/2.42b16017.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b678142f649803aed036",
+    "url": "/google-books-search/static/js/main.07a9195f.chunk.js"
+  },
+  {
+    "revision": "821df562f8f722ae59b4",
+    "url": "/google-books-search/static/js/runtime-main.3b9f7c91.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/google-books-search/static/media/logo.5d5d9eef.svg"
+  }
+]);
